7872e4c61513bcb293e386fc10a17c80f52083b0
tbberhe <114125346+tbberhe@users.noreply.github.com>
2022-10-31 22:35:55 -0700

    Add files via upload

---
+++ tbhw6/scripts.js
@@ -0,0 +1,15 @@
+const dayjs = require('dayjs')
+//import dayjs from 'dayjs' // ES 2015
+dayjs().format()
+{ //tsconfig.json
+    "compilerOptions": {
+      "esModuleInterop": true,
+      "allowSyntheticDefaultImports": true,
+    }
+    dayjs.extend(localeData)
+
+dayjs.weekdays("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+dayjs.weekdaysShort(5)
+dayjs.weekdaysMin(5)
+dayjs.monthsShort(12)
+dayjs.months('January', 'February', 'March', "April", 'May', 'June', 'July', 'Augst', 'September', 'October', 'November', 'December') ;
